3e3b534c13a847e7ac914c5fbec7ab80bb300ae9
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-27 00:54:09 +0900

    studentモデルをuse createStudent作成

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
+// use Request;
+use DB;
+use Log;
 
 class ApiController extends Controller
 {
+
     public function getAllStudents()
     {
         # code...
@@ -13,7 +18,27 @@ class ApiController extends Controller
 
     public function createStudent(Request $request)
     {
-        # code...
+        Log::info("request:$request");
+        // logger('test', ['foo' => 'bar']);
+        // データベースに保存
+        DB::transaction(function () {
+            $student = new Student;
+            $student->name   = $request->name;
+            $student->course = $request->course;
+            $student->save();
+        });
+        //jsonで返す
+        return response()->json([
+            "message" => "student record created"
+        ],201);
+        // 201 は作成成功のステータスコード
+        // try {
+        // } catch (\Throwable $th) {
+        //     return response()->json([
+        //         "message" => "error"
+        //     ],500);
+        // }
+
     }
 
     public function getStudent($id)
