23ffac245d923bda7760c55385235021b37df581
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-26 22:53:00 +0900

    必要な関数宣言だけ

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function getAllStudents()
+    {
+        # code...
+    }
+
+    public function createStudent(Request $request)
+    {
+        # code...
+    }
+
+    public function getStudent($id)
+    {
+        # code...
+    }
+
+    public function updateStudent(Request $request,$id)
+    {
+        # code...
+    }
+
+    public function deleteStudent($id)
+    {
+        # code...
+    }
+}
